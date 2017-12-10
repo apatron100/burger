54e34010688db0f93f94af4c52346cd628de56ac
alfredo patron <apatron100@cox.net>
2017-12-09 17:49:59 -0800

    more code

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,17 @@
-var connection = require("connection.js");
+var connection = require("./connection.js");
+
+var orm = {
+	all: function(tableInput, cb){
+		connection.query('SELECT * FROM ' +tableInput+';', function(err, result){
+			if(err) throw err;
+			cb(result)
+		} )
+	},
+	update: function(table, condition, cb){
+		connection.query('update ' +TableInput+' SET devoured=true where id=' +condition+';', function(err,result){
+			if(err)throw err;
+			cb(result);
+		})
+	}
+}
+module.export = orm;
